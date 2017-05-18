0d18a153b6954c19b269b5a088844421b0dd68ae
lake4790k <lake4790k@users.noreply.github.com>
2017-05-18 23:52:35 +0200

    FastFindLanes

--- Main.py
+++ Main.py
@@ -10,31 +10,56 @@ logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%H:
 logging.debug('Advanced lane finding initializing')
 
 
+class Data:
+    def __init__(self):
+        self.rawImg = None
+        self.undistImg = None
+        self.binaryImg = None
+        self.warpedImg = None
+
+        self.M = None
+        self.Minv = None
+
+        self.ploty = np.array([])
+        self.left_fitx = np.array([])
+        self.right_fitx = np.array([])
+        self.left_fit = np.array([])
+        self.right_fit = np.array([])
+
+        self.left_curverad = None
+        self.right_curverad = None
+
+
 class Processor:
+    def __init__(self, data: Data):
+        self.data = data
 
-    def process(self):
-        pass
+    def process(self, img: np.array) -> np.array:
+        return img
 
-    def show2(self, img, img_):
+    def show(self, img: np.array) -> np.array:
+        img_ = self.process(img)
+        # cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.show2(img, img_)
+        return img_
+
+    def show2(self, img: np.array, img_: np.array):
         f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
         ax1.set_title('original')
-        ax1.imshow(img)
+        if len(img.shape) < 3:
+            ax1.imshow(img, cmap='gray')
+        else:
+            ax1.imshow(img)
         ax2.set_title('processed')
         if len(img_.shape) < 3:
             ax2.imshow(img_, cmap='gray')
         else:
             ax2.imshow(img_)
 
-    def show(self, img):
-        img_ = self.process(img)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.show2(img, img_)
-
 
 class Calibration(Processor):
-
-    def __init__(self):
-        Processor.__init__(self)
+    def __init__(self, data: Data):
+        Processor.__init__(self, data)
         self.mtx = []
         self.dist = []
 
@@ -54,8 +79,10 @@ class Calibration(Processor):
         imgpoints = []
         images = glob.glob('./camera_cal/calibration*.jpg')
 
+        img_size = 0
         for fname in images:
             img = cv2.imread(fname)
+            img_size = img.shape[0:2]
             logging.debug('calibrating %s', fname)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
@@ -63,7 +90,6 @@ class Calibration(Processor):
                 objpoints.append(objp)
                 imgpoints.append(corners)
 
-        img_size = img.shape[0:2]
         _, self.mtx, self.dist, _, _ = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
         logging.info('Computed calibration matrix')
         with open('./camera_cal/calib.mtx', 'wb') as f:
@@ -71,18 +97,17 @@ class Calibration(Processor):
         with open('./camera_cal/calib.dist', 'wb') as f:
             pickle.dump(self.dist, f)
 
-    def process(self, img):
+    def process(self, img: np.array) -> np.array:
         return cv2.undistort(img, self.mtx, self.dist, None, self.mtx)
 
 
 class BinaryThreshold(Processor):
-
-    def __init__(self, s_thresh=(170, 255), sx_thresh=(20, 100)):
-        Processor.__init__(self)
+    def __init__(self, data: Data, s_thresh=(170, 255), sx_thresh=(20, 100)):
+        Processor.__init__(self, data)
         self.s_thresh = s_thresh
         self.sx_thresh = sx_thresh
 
-    def process(self, img):
+    def process(self, img: np.array) -> np.array:
         hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
         l_channel = hsv[:, :, 1]
         s_channel = hsv[:, :, 2]
@@ -104,33 +129,38 @@ class BinaryThreshold(Processor):
 
 
 class PerspectiveTransform(Processor):
-
-    def __init__(self):
-        Processor.__init__(self)
+    def __init__(self, data: Data):
+        Processor.__init__(self, data)
         self.src = np.float32([[550, 450], [750, 450], [1200, 700], [150, 700]])
         self.dst = np.float32([[0, 0], [1280, 0], [1280, 720], [0, 720]])
-        self.M = cv2.getPerspectiveTransform(self.src, self.dst)
-        self.Minv = cv2.getPerspectiveTransform(self.dst, self.src)
+        self.data.M = cv2.getPerspectiveTransform(self.src, self.dst)
+        self.data.Minv = cv2.getPerspectiveTransform(self.dst, self.src)
 
-    def process(self, img):
-        return cv2.warpPerspective(img, self.M, (img.shape[1], img.shape[0]))
+    def process(self, img: np.array) -> np.array:
+        return cv2.warpPerspective(img, self.data.M, (img.shape[1], img.shape[0]))
 
-    def show(self, img):
+    def show(self, img: np.array) -> np.array:
         img_ = self.process(img)
-        if len(img.shape)>2:
+        if len(img.shape) > 2:
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             img_ = cv2.cvtColor(img_, cv2.COLOR_BGR2RGB)
             cv2.polylines(img, np.int32([self.src]), True, [255, 255, 255], 5)
+
         self.show2(img, img_)
+        return img_
 
 
 class FindLanes(Processor):
-
-    def __init__(self, nwindows=9):
-        Processor.__init__(self)
+    def __init__(self, data: Data, nwindows=9):
+        Processor.__init__(self, data)
         self.nwindows = nwindows
+        self.nonzeroy = None
+        self.nonzerox = None
+        self.out_img = None
+        self.left_lane_inds = None
+        self.right_lane_inds = None
 
-    def process(self, img):
+    def process(self, img: np.array) -> np.array:
         histogram = np.sum(img[img.shape[0] // 2:, :], axis=0)
         self.out_img = np.dstack((img, img, img)) * 255
 
@@ -185,85 +215,105 @@ class FindLanes(Processor):
         rightx = self.nonzerox[self.right_lane_inds]
         righty = self.nonzeroy[self.right_lane_inds]
 
-        self.left_fit = np.polyfit(lefty, leftx, 2)
-        self.right_fit = np.polyfit(righty, rightx, 2)
-
-    def show(self, img):
-        self.process(img)
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
 
         ploty = np.linspace(0, img.shape[0] - 1, img.shape[0])
-        left_fitx = self.left_fit[0] * ploty ** 2 + self.left_fit[1] * ploty + self.left_fit[2]
-        right_fitx = self.right_fit[0] * ploty ** 2 + self.right_fit[1] * ploty + self.right_fit[2]
+        self.data.left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+        self.data.right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+        self.data.ploty = ploty
+        self.data.left_fit = left_fit
+        self.data.right_fit = right_fit
+        return img
+
+    def show(self, img: np.array):
+        self.process(img)
 
         self.out_img[self.nonzeroy[self.left_lane_inds], self.nonzerox[self.left_lane_inds]] = [255, 0, 0]
         self.out_img[self.nonzeroy[self.right_lane_inds], self.nonzerox[self.right_lane_inds]] = [0, 0, 255]
 
         plt.figure(figsize=(20, 6))
         plt.imshow(self.out_img)
-        plt.plot(left_fitx, ploty, color='yellow')
-        plt.plot(right_fitx, ploty, color='yellow')
+        plt.plot(self.data.left_fitx, self.data.ploty, color='yellow')
+        plt.plot(self.data.right_fitx, self.data.ploty, color='yellow')
         plt.xlim(0, 1280)
         plt.ylim(720, 0)
 
 
-class FastFindLanes:
+class NextFindLanes(Processor):
+    def __init__(self, data: Data):
+        Processor.__init__(self, data)
+        self.margin = 100
+        self.nonzeroy = None
+        self.nonzerox = None
+        self.left_lane_inds = None
+        self.right_lane_inds = None
+        self.leftx = None
+        self.rightx = None
+        self.lefty = None
 
-    def __init__(self):
-        Processor.__init__(self)
-
-    def process(self, img):
+    def process(self, img: np.array) -> np.array:
         nonzero = img.nonzero()
         self.nonzeroy = np.array(nonzero[0])
         self.nonzerox = np.array(nonzero[1])
 
-        margin = 100
-        left_lane_inds = \
-                        ((self.nonzerox > (self.left_fit[0] * (self.nonzeroy ** 2) +
-                                            self.left_fit[1] * self.nonzeroy +
-                                            self.left_fit[2] - margin)) &
-                        (self.nonzerox < (self.left_fit[0] * (self.nonzeroy ** 2) +
-                                          self.left_fit[1] * self.nonzeroy +
-                                          self.left_fit[2] + margin)))
+        left_fit = self.data.left_fit
+        right_fit = self.data.right_fit
 
-        right_lane_inds = \
-                        ((self.nonzerox > (self.right_fit[0] * (self.nonzeroy ** 2) +
-                                           self.right_fit[1] * self.nonzeroy +
-                                           self.right_fit[2] - margin)) &
-                        (self.nonzerox < (self.right_fit[0] * (self.nonzeroy ** 2) +
-                                          self.right_fit[1] * self.nonzeroy +
-                                          self.right_fit[2] + margin)))
+        self.left_lane_inds = \
+            ((self.nonzerox > (left_fit[0] * (self.nonzeroy ** 2) +
+                               left_fit[1] * self.nonzeroy +
+                               left_fit[2] - self.margin)) &
+             (self.nonzerox < (left_fit[0] * (self.nonzeroy ** 2) +
+                               left_fit[1] * self.nonzeroy +
+                               left_fit[2] + self.margin)))
 
-        leftx = self.nonzerox[left_lane_inds]
-        lefty = self.nonzeroy[left_lane_inds]
-        rightx = self.nonzerox[right_lane_inds]
-        righty = self.nonzeroy[right_lane_inds]
+        self.right_lane_inds = \
+            ((self.nonzerox > (right_fit[0] * (self.nonzeroy ** 2) +
+                               right_fit[1] * self.nonzeroy +
+                               right_fit[2] - self.margin)) &
+             (self.nonzerox < (right_fit[0] * (self.nonzeroy ** 2) +
+                               right_fit[1] * self.nonzeroy +
+                               right_fit[2] + self.margin)))
 
-        left_fit = np.polyfit(lefty, leftx, 2)
-        right_fit = np.polyfit(righty, rightx, 2)
+        self.leftx = self.nonzerox[self.left_lane_inds]
+        lefty = self.nonzeroy[self.left_lane_inds]
+        self.rightx = self.nonzerox[self.right_lane_inds]
+        righty = self.nonzeroy[self.right_lane_inds]
+
+        self.data.left_fit = np.polyfit(lefty, self.leftx, 2)
+        self.data.right_fit = np.polyfit(righty, self.rightx, 2)
 
         ploty = np.linspace(0, img.shape[0] - 1, img.shape[0])
-        left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
-        right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+        self.data.left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+        self.data.right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+        self.data.ploty = ploty
+        return img
 
-    def show(self, img):
+    def show(self, img: np.array):
         self.process(img)
 
         out_img = np.dstack((img, img, img)) * 255
         window_img = np.zeros_like(out_img)
 
-        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+        out_img[self.nonzeroy[self.left_lane_inds], self.nonzerox[self.left_lane_inds]] = [255, 0, 0]
+        out_img[self.nonzeroy[self.right_lane_inds], self.nonzerox[self.right_lane_inds]] = [0, 0, 255]
 
-        left_line_window1 = np.array([np.transpose(np.vstack([left_fitx - margin, ploty]))])
-        left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx + margin, ploty])))])
+        left_fitx = self.data.left_fitx
+        right_fitx = self.data.right_fitx
+        ploty = self.data.ploty
+        left_line_window1 = np.array([np.transpose(np.vstack([left_fitx - self.margin, ploty]))])
+        left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx + self.margin, ploty])))])
         left_line_pts = np.hstack((left_line_window1, left_line_window2))
-        right_line_window1 = np.array([np.transpose(np.vstack([right_fitx - margin, ploty]))])
-        right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx + margin, ploty])))])
+
+        right_line_window1 = np.array([np.transpose(np.vstack([right_fitx - self.margin, ploty]))])
+        right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx + self.margin, ploty])))])
         right_line_pts = np.hstack((right_line_window1, right_line_window2))
 
         cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
         cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
         result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+        plt.figure(figsize=(20, 6))
         plt.imshow(result)
         plt.plot(left_fitx, ploty, color='yellow')
         plt.plot(right_fitx, ploty, color='yellow')
@@ -271,9 +321,28 @@ class FastFindLanes:
         plt.ylim(720, 0)
 
 
-if __name__ == '__main__':
+class Curvature(Processor):
+    def __init__(self, data: Data):
+        Processor.__init__(self, data)
+        self.ym_per_pix = 30 / 720
+        self.xm_per_pix = 3.7 / 700
 
-    calibration = Calibration()
-    thresholded = BinaryThreshold()
-    perspective = PerspectiveTransform()
-    findLanes = FindLanes()
+    def process(self, img: np.array) -> np.array:
+        ploty = self.data.ploty
+        y_eval = np.max(ploty)
+        left_fit_cr = np.polyfit(ploty * self.ym_per_pix, self.data.left_fitx * self.xm_per_pix, 2)
+        right_fit_cr = np.polyfit(ploty * self.ym_per_pix, self.data.right_fitx * self.xm_per_pix, 2)
+
+        self.data.left_curverad = ((1 + (2 * left_fit_cr[0] * y_eval * self.ym_per_pix + left_fit_cr[1]) ** 2) ** 1.5) / \
+                                    np.absolute(2 * left_fit_cr[0])
+        self.data.right_curverad = ((1 + (2 * right_fit_cr[0] * y_eval * self.ym_per_pix + right_fit_cr[1]) ** 2) ** 1.5) / \
+                                    np.absolute(2 * right_fit_cr[0])
+        return img
+
+    def show(self, img: np.array):
+        self.process(img)
+        print(self.data.left_curverad, 'm', self.data.right_curverad, 'm')
+
+
+if __name__ == '__main__':
+    logging.info("STARTING")
